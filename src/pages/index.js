f2574ccbdee720f49f3082931bf0559b08a36a0c
IsioPablo <isiopawel@gmail.com>
2022-05-03 01:25:57 +0100

    Gatsby tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I am making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="A legend of the highest caliber"
+        src="../images/lol.jpg"
+      />
     </Layout>
   )
 }
